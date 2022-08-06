653117216d00e6ee16f38bc062887ae03e676b66
deneb1593 <deneb1593@gmail.com>
2022-08-06 14:11:41 +0900

    사용자 추가, 인증여부 수정 단위 테스트 추가

--- src/test/java/org/sample/login/user/UserRepositoryTest.java
+++ src/test/java/org/sample/login/user/UserRepositoryTest.java
@@ -12,6 +12,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -21,10 +24,12 @@ public class UserRepositoryTest {
     @MockBean
     private UserRepository userRepository;
 
+    private static final String TEST_EMAIL = "test@gmail.com";
+    private static final int UPDATE_SUCCESS = 1;
+
     @Test
     @DisplayName("사용자_조회_테스트")
     public void userSelectTest() {
-        final String TEST_EMAIL = "test@gmail.com";
 
         User user = User.builder()
                 .email(TEST_EMAIL)
@@ -45,12 +50,34 @@ public class UserRepositoryTest {
     @Test
     @DisplayName("사용자_추가_테스트")
     public void userSaveTest() {
+        when(userRepository.save(any(User.class)))
+                .then(returnsFirstArg());
 
+
+        User user = User.builder()
+                .email(TEST_EMAIL)
+                .password("1234")
+                .username("kim")
+                .name("park")
+                .userRole(UserRole.USER)
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        Assertions.assertEquals(TEST_EMAIL, savedUser.getEmail());
+        Assertions.assertEquals("1234", savedUser.getPassword());
+
+        verify(userRepository).save(any());
     }
 
     @Test
     @DisplayName("사용자_인증여부_수정_테스트")
     public void userEnableTest() {
+        when(userRepository.enableUser(TEST_EMAIL))
+                .thenReturn(UPDATE_SUCCESS);
 
+        int result = userRepository.enableUser(TEST_EMAIL);
+
+        Assertions.assertEquals(UPDATE_SUCCESS, result);
     }
 }
